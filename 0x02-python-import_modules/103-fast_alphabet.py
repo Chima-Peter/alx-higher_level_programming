01cf2496a7e6cbbbdb581fbe9d309d887767a79c
Chima-Peter <chimaojimma@gmail.com>
2022-12-01 16:10:02 -0800

    101 and 103

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-        print("{}".format(i), end="") if (i != "Z") else print("{}".format("Z"))
+    import alpha
