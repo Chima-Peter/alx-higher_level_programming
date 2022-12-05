c3d96f532570f455c9458f56fc815e3ce9832552
Chima-Peter <chimaojimma@gmail.com>
2022-12-05 14:21:17 -0800

    taslks 0, 3 and 6

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    num = len(my_list)
-    count = 1
-    for i in my_list:
-        n = num - count
-        print("{}".format(my_list[n]))
-        count += 1
+    my_list.reverse()
+    for i in range(0, len(my_list)):
+        print("{}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     new = []
-    for n in range(3):
-        new.append([i[n] for i in matrix])
-    print("{}".format(new))
+    for i in matrix:
+        x = 0
+        for n in i:
+            print("{}".format(n), end="")
+            x += 1
+            if x < 3:
+                print(end=" ")
+        print()
+#    print(len(matrix)
