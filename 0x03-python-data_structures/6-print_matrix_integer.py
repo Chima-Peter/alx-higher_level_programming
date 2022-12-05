4af7f3eb769785a52101ab5168b85bd5d5957fd5
Chima-Peter <chimaojimma@gmail.com>
2022-12-05 14:29:06 -0800

    works now

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         x = 0
         for n in i:
-            print("{}".format(n), end="")
+            print("{:d}".format(n), end="")
             x += 1
             if x < 3:
                 print(end=" ")
