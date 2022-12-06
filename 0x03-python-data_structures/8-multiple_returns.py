af1d490dce7ef02f7f6f845bbba4db8c44733df1
Chima-Peter <chimaojimma@gmail.com>
2022-12-05 17:49:27 -0800

    changed some

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     num = len(sentence)
-    first = sentence[0]
-    new_tuple = (num, first)
-    return new_tuple
+    if num > 0:
+        first = sentence[0]
+        new_tuple = (num, first)
+        return new_tuple
+    else:
+        first = None
+        new_tuple = (num, first)
+        return new_tuple
