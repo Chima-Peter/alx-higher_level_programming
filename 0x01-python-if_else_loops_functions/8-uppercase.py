71f29d36b26e7443bef4e11c2337f3cc46a44b39
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-08 19:24:47 -0700

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 97 and ord(i) < 123:
+            new = ord(i) - 32
+        else:
+            new = ord(i)
+        print("{}".format(chr(new)), end="")
+    print()
