c91cb43ae7df9d54bd616c3cbe9175f0d17a360f
Chima Ojimma <chimaojimma@gmail.com>
2023-08-09 01:29:34 +0000

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+	for n in range(1, 10):
+		if i < n and i != 8:
+			print("{}{}".format(i, n), end=", ")
+		if i == 8 and n == 9:
+			print("{}{}".format(i, n))
