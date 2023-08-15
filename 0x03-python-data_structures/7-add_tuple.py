8c24f1774baf9f0ceeeed03b0427be39e20c5808
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-14 18:29:30 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,4 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0:
         a = (0, 0)
@@ -10,7 +10,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) == 0:
         b = (0, 0)
     elif len(tuple_b) == 1:
-        b = (tuple_b, 0)
+        b = (tuple_b[0], 0)
     else:
         b = (tuple_b[0], tuple_b[1])
 
