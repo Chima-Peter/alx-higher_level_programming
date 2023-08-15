6e850731ad0331804e71c83110a77b838349c87f
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-14 19:30:26 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list []
-    for i in range(0, len(my_list)):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = my_list
+    my_list = []
+    for i in range(0, len(new_list)):
         if i != idx:
-            new_list.append(my_list[i])
-    my_list = new_list
+            my_list.append(new_list[i])
     return my_list
