82062878bdf43b18b643e8781541a5096d894094
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-28 10:57:35 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a / b
     except Exception:
         return None
     finally:
-        print("Inside result: {:d}".format(result))
+        print("Inside result: {}".format(result))
+        return result
