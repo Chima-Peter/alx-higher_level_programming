1eba5d490f71f8024c16dc7f00f3edd0151e370c
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-29 12:32:01 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Creates a class Square that's based on 0-square.py"""
+
+
+class Square:
+    """Defines a class that's initialised with a private attribute"""
+
+    __init__(self, size):
+        """Initialises attributes for the Square class
+
+        Args:
+            size: the size of the class square
+        """
+        self.__size = size
+
