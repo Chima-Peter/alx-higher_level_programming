2fdfb2ba206680a3b2f11aafe9bc2ff7c565be59
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-08-29 12:40:20 -0700

    0x06-python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,11 +5,9 @@
 class Square:
     """Defines a class that's initialised with a private attribute"""
 
-    __init__(self, size):
+    def __init__(self, size):
         """Initialises attributes for the Square class
-
-        Args:
-            size: the size of the class square
+    Args:
+        size: the size of the class square
         """
         self.__size = size
-
