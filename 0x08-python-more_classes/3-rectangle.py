b44b404ee589e909502b76d6e9d149d206fe938a
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-04 17:22:53 -0700

    0x06-python-classes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -103,7 +103,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ''
         for i in range(self.__height):
-            for n in range(self.__height):
+            for n in range(self.__width):
                 print("#", end="")
             print()
         return ''
