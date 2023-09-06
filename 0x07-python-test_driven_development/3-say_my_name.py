d4ae18c071aac8fe76b653fd64729de0150ace3c
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-06 07:02:10 -0700

    PYTHON TASKS

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Write a function that prints My name is <first name> <last name>
+Prototype: def say_my_name(first_name, last_name="")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function that prints My name is <first name> <last name>
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {}  {}".format(first_name, last_name))
