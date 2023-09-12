e1c247607270c60b7248c7fb221101e46b9a75bb
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-11 20:17:32 -0700

    0x06-python-classes

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that inherits from Rectangle (9-rectangle.py)
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class that inherits Rectangle from 9-rectangle
+    """
+    def __init__(self, size):
+        """
+        Initialises the class witn size
+        """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Print to output whenever str or print functions are called on the class
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
