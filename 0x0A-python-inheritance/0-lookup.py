e465606720cd018c0e456bd5d9330a6e21de0beb
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-11 19:09:41 -0700

    0x06-python-classes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,9 +4,8 @@ Module taht creates the lookup function.
 """
 
 
-
 def lookup(obj):
     """
     A function that returns a attributes and methods of an object.
     """
-    return obj.dir()
+    return dir(obj)
