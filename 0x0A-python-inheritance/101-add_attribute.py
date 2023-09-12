20de9daa99108a210cd604a662dc047a8264a458
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-11 20:37:41 -0700

    0x06-python-classes

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that creates the add_attribute function
+"""
+
+
+def add_attribute(obj, name, value):
+    """
+    Tries to add a new attribute to an object if possible
+    """
+    setattr(obj, name, value)
+    if hasattr(name) is False:
+        raise TypeError("can't add new attribute)
