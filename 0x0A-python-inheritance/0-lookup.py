4b7a61a665919ecffc9862b9a6462ffd7ee66214
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-11 19:08:18 -0700

    0x06-python-classes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module taht creates the lookup function.
+"""
+
+
+
+def lookup(obj):
+    """
+    A function that returns a attributes and methods of an object.
+    """
+    return obj.dir()
