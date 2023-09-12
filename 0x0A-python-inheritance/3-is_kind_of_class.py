80a361c1a3038b1f9f7ebcf870453d58dd4183db
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-11 19:31:22 -0700

    0x06-python-classes

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that creates the is_kind_of_class function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that checks if obj is an instance of or if obj is a\
+            subclass of the specified class
+    """
+    if isinstance (obj, a_class) == True:
+        return True
+    else:
+        return False
