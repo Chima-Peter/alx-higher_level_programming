fd3e729c1cfd75438896396f49d1d38f18f25111
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-11 19:47:36 -0700

    0x06-python-classes

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,17 @@
-#!/usr/bin.python3
+#!/usr/bin/python3
 """
-Module that creates a class BaseGeometry
+Module that creates a class BaseGeometry based on 5-base_geometry
 """
 
 
 class BaseGeometry:
     """
-    Creates an empty class
+    Improves the BaseGeometry class
+    Args:
+        Public instance method: def area(self)
     """
-    pass
+    def area(self):
+        """
+        A function hat raises an Exception
+        """
+        raise Exception("area() is not implemented")
