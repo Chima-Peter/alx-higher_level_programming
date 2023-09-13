7ba6046da4ea290bf04f3d5e85009c49aec8cc3e
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-12 19:42:54 -0700

    0x06-python-classes

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,16 @@ Module that uses save_to_json_file and load_from_json_file functions
 """
 import sys
 import json
+import os.path
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
-import os.path
 
-is os.path.exists('add_item.json') is False:
-    print("Error")
+my_list = []
+f = 'add_item.json'
+if os.path.exists(f) is False:
+    print("P")
+    save(my_list, f)
+my_list = load(f)
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+save(my_list, f)
