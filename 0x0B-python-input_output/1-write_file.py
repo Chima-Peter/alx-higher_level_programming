ed7013dbc04ca798c668f55070030dbc1b0dd6c4
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-12 18:25:51 -0700

    0x06-python-classes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,7 +6,8 @@ Module for the def write_file function
 
 def write_file(filename="", text=""):
     """
-    A function that writes a string to a text file (UTF8) and returns the number of characters written
+    A function that writes a string to a text file (UTF8)\
+            and returns the number of characters written
     """
-    with open(filename, 'r+', encoding='utf-8') as f:
+    with open(filename, 'w+', encoding='utf-8') as f:
         return f.write(text)
