c70b607b3ba1ff8650f826b96b364b055df837a1
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-12 19:52:36 -0700

    0x06-python-classes

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module that creates a class Student
+"""
+
+
+class Student:
+    """
+    A class Student that defines a student by:
+    Args:
+        Public instance attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialisation of class with first_name, last_name and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictioanry representaion of a Student instance
+        """
+        return Student.__dict__
