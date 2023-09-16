76ede077fccc9c957af7f51f25e5009d54540cd7
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-09-16 07:33:58 -0700

    ALMOST_A_CIRCLE

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!usr/bin/python3
+"""
+Module that creates the first class Base
+"""
+
+
+class Base:
+    """
+    Class Base with:
+        private class attribute __nb_objects
+        class constructor __init
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """
+        Class contructor for Base, increments __nb_objects and set\
+                value to id
+        """
+        if id != None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
