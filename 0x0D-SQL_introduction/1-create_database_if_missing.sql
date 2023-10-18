983dd8acf2472fa9983765c2f3f4334d39895f71
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-10-17 19:18:32 -0700

    SQL_INTRODUCTION

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,8 @@
+-- a script that creates the database hbtn_0c_0 in your MySQL server
+use sys;
+CREATE TABLE IF NOT EXISTS hbtn_0c_0 (
+	FirstName varchar(255),
+	LastName varchar(255),
+	Age int,
+	id int
+);
