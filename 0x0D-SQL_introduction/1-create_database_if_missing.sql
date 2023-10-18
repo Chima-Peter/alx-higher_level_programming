e18005cfde03b86ef4c702e1873f7bb991c584bf
CHIMA OJIMMA <chimaojimma@gmail.com>
2023-10-17 19:34:12 -0700

    SQL_INTRODUCTION

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,8 +1,2 @@
 -- a script that creates the database hbtn_0c_0 in your MySQL server
-use sys;
-CREATE TABLE IF NOT EXISTS hbtn_0c_0 (
-	FirstName varchar(255),
-	LastName varchar(255),
-	Age int,
-	id int
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
